e05814a4dc262afdcdbdb254717395c0331595f9
ReetamBis <reetam.biswas33@gmail.com>
2020-10-14 04:53:44 +0530

    PandoraApp

--- app/src/main/java/com/example/pandora/LoginActivity.java
+++ app/src/main/java/com/example/pandora/LoginActivity.java
@@ -1,26 +1,112 @@
 package com.example.pandora;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText roll;
+    EditText mail;
     EditText pass;
     Button signin;
     TextView reg;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
+    public boolean checkpassword(String s)
+    {
+
+        int n=s.length();
+        if(n<8) {
+            Toast.makeText(LoginActivity.this, "Minimum password length must be 8 !!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        int c=0,num=0;
+        String specialCharactersString = "!@#$%&*()'+,-./:;<=>?[]^_`{|}";
+        for(int i=0;i<n;i++)
+        {
+            char ch = s.charAt(i);
+            if(specialCharactersString.contains(Character.toString(ch))) {
+                c=c+1;
+
+            }
+            else if(Character.isDigit(s.charAt(i)))
+            {
+                num=num+1;
+            }
+
+        }
+        if(num==0||c==0)
+        {
+            Toast.makeText(LoginActivity.this, "Password must contain minimum one digit and one symbol !!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
+
+    }
+
+    public void checkUsersAccessLevel(String uid){
+
+        DocumentReference df = fStore.collection("Users").document(uid);
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d("TAG","onSuccess: "+ documentSnapshot.getData());
+
+                if(documentSnapshot.getString("isAdmin") != null){
+                    //admin intent
+                }
+
+                if(documentSnapshot.getString("isUser") != null){
+                    Intent intent1= new Intent(LoginActivity.this,com.example.pandora.DashBoard.class);
+                    startActivity(intent1);
+                    LoginActivity.this.finish();
+                }
+            }
+        });
+    }
 
     public void onClick(View view){
 
         if(view.getId()==R.id.signin){
 
-            LoginActivity.this.finish();
+            if(checkpassword(pass.getText().toString())){
+
+                fAuth.signInWithEmailAndPassword(mail.getText().toString(),pass.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+
+                        Toast.makeText(LoginActivity.this, "Successfully Logged In !!", Toast.LENGTH_LONG).show();
+                        checkUsersAccessLevel(authResult.getUser().getUid());
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+                LoginActivity.this.finish();
+            }
+            else
+                Toast.makeText(LoginActivity.this, "Wrong UserId or Password !!", Toast.LENGTH_LONG).show();
         }
 
         if(view.getId()==R.id.reg){
@@ -34,11 +120,23 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        roll = findViewById(R.id.roll);
+        mail = findViewById(R.id.mail);
         pass = findViewById(R.id.pass);
         signin = findViewById(R.id.signin);
         reg = findViewById(R.id.reg);
 
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+            Intent intent1= new Intent(LoginActivity.this,com.example.pandora.DashBoard.class);
+            startActivity(intent1);
+            LoginActivity.this.finish();
+        }
     }
 }
--- app/src/main/java/com/example/pandora/Register.java
+++ app/src/main/java/com/example/pandora/Register.java
@@ -1,9 +1,24 @@
 package com.example.pandora;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
@@ -13,11 +28,131 @@ public class Register extends AppCompatActivity {
     EditText email;
     EditText pass;
     EditText repass;
+    Button register;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
 
+     boolean checkmail(String mail,String roll){
+
+        for(int i =0;i<7;i++){
+
+            if(mail.charAt(i)!=roll.charAt(i)){
+                Toast.makeText(Register.this, "Mail doesn't match with roll !!", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        }
+
+        if(!mail.substring(8).equals("kiit.ac.in")){
+            Toast.makeText(Register.this, "Please enter your KIIT mail ID !!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean checkpassword(String s)
+    {
+
+        int n=s.length();
+        if(n<8) {
+            Toast.makeText(Register.this, "Minimum password length must be 8 !!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        int c=0,num=0;
+        String specialCharactersString = "!@#$%&*()'+,-./:;<=>?[]^_`{|}";
+        for(int i=0;i<n;i++)
+        {
+            char ch = s.charAt(i);
+            if(specialCharactersString.contains(Character.toString(ch))) {
+                c=c+1;
+
+            }
+            else if(Character.isDigit(s.charAt(i)))
+            {
+                num=num+1;
+            }
+
+        }
+        if(num==0||c==0)
+        {
+            Toast.makeText(Register.this, "Password must contain minimum one digit and one symbol !!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
+
+    }
+
+    public boolean allfilled(){
+
+         if(id.getText().toString().equals("") || name.getText().toString().equals("") ||
+                 num.getText().toString().equals("") || email.getText().toString().equals("") ||
+                 pass.getText().toString().equals("") || repass.getText().toString().equals("")) {
+             Toast.makeText(Register.this, "Fill all the fields !!", Toast.LENGTH_LONG).show();
+             return false;
+         }
+         return true;
+    }
+
+    public boolean match(){
+
+         if(!pass.getText().toString().equals(repass.getText().toString())){
+             Toast.makeText(Register.this, "Password does not match !!", Toast.LENGTH_LONG).show();
+             return false;
+         }
+         return true;
+    }
+
+    public void onReg(View view){
+
+        if(view.getId()==R.id.register){
+
+            if(allfilled() && checkmail(email.getText().toString(),id.getText().toString()) && checkpassword(pass.getText().toString())
+                    && match()){
+                fAuth.createUserWithEmailAndPassword(email.getText().toString(),pass.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        FirebaseUser user = fAuth.getCurrentUser();
+                        Toast.makeText(Register.this, "Account Created", Toast.LENGTH_SHORT).show();
+                        DocumentReference df = fStore.collection("Users").document(user.getUid());
+                        Map<String,Object> userInfo = new HashMap<>();
+
+                        userInfo.put("UserName",name.getText().toString());
+                        userInfo.put("Roll",id.getText().toString());
+                        userInfo.put("E-Mail",email.getText().toString());
+                        userInfo.put("Mobile",num.getText().toString());
+
+                        userInfo.put("isUser","1");
+                        df.set(userInfo);
+
+                        Register.this.finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Register.this, "Failed to Create !!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        id = findViewById(R.id.id);
+        name = findViewById(R.id.name);
+        num = findViewById(R.id.num);
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.pass);
+        repass = findViewById(R.id.repass);
+        register = findViewById(R.id.register);
+
     }
 }
