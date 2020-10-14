d9492c889cbe327f4987e4be0198c7265dada0d3
mudit4158 <mudit2812@gmail.com>
2020-10-14 22:08:31 +0530

    Mudit:added tab Structure(2)

--- app/src/main/java/com/example/pandora/DashBoard.java
+++ app/src/main/java/com/example/pandora/DashBoard.java
@@ -1,14 +1,40 @@
 package com.example.pandora;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class DashBoard extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
 
+public class DashBoard extends AppCompatActivity {
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        adapter = new TabAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new HomeFragment(), "Home");
+        adapter.addFragment(new AddPostFragment(), "Add");
+        adapter.addFragment(new SearchFragment(), "Search");
+        adapter.addFragment(new AccountFragment(), "Account");
+        int[] tabIcons = {
+                R.drawable.home_icon,
+                R.drawable.search_icon,
+                R.drawable.add_icon,
+                R.drawable.user_icon
+        };
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+        tabLayout.getTabAt(3).setIcon(tabIcons[3]);
     }
+
 }
--- app/src/main/java/com/example/pandora/TabAdapter.java
+++ app/src/main/java/com/example/pandora/TabAdapter.java
@@ -14,20 +14,24 @@ public class TabAdapter extends FragmentStatePagerAdapter {
     private final List<Fragment> mFragmentList = new ArrayList<>();
     private final List<String> mFragmentTitleList = new ArrayList<>();
 
-    public TabAdapter(@NonNull FragmentManager fm, int behavior) {
-        super(fm, behavior);
+    public TabAdapter(@NonNull FragmentManager fm) {
+        super(fm);
     }
     @NonNull
     @Override
     public Fragment getItem(int position) {
         return mFragmentList.get(position);
-
     }
 
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        return mFragmentTitleList.get(position);
+        return null;
+        //return mFragmentTitleList.get(position);
+    }
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
     }
 
     @Override
